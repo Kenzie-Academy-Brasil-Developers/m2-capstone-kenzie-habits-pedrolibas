62a5d4a363e92b912bfa352ef345131e236020bf
Henrique Carderan <rike.c.12@gmail.com>
2022-07-13 09:41:01 -0300

    feat: branch criada para criar modal criar

---
+++ src/scripts/modalCriar.js
@@ -0,0 +1,31 @@
+class ModalCriar {
+    static criar() {
+        const body = document.querySelector('body')
+        const modalCriar = document.createElement("div")
+        const container = document.createElement("div")
+        const modalInnerCriar = document.createElement("div")
+        const modalCriarCabecalho = document.createElement("div")
+        const modalCriarStatus = document.createElement("div")
+        const modalCriarBotoes = document.createElement("div")
+        const h2 = document.createElement("h2")
+        const i = document.createElement("i")
+        const btnSair = document.createElement("button")
+        const form = document.createElement("form")
+        const titulo = document.createElement("label")
+        const textoTitulo = document.createElement("input")
+        const descricao = document.createElement("label")
+        const textoDescricao = document.createElement("textarea")
+        const categoria = document.createElement("label")
+        const selecao = document.createElement("select")
+        const casa = document.createElement("option")
+        const estudo = document.createElement("option")
+        const lazer = document.createElement("option")
+        const trabalho = document.createElement("option")
+        const saude = document.createElement("option")
+        const status = document.createElement("label")
+        const check = document.createElement("input")
+        const btnInserir = document.createElement("button")
+
+        
+    }
+}
